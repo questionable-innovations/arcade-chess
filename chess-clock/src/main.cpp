df86482c8f146bea4550b3cab1ec2389d9b3923f
Arushi Gupta <arushigupta314159@gmail.com>
2026-07-15 23:36:34 +1200

    Update initial file as a scrolling 'hello' render based on an exemplar

--- chess-clock/src/main.cpp
+++ chess-clock/src/main.cpp
@@ -1,41 +1,9 @@
-// #include <Arduino.h>
-// #include <MD_Parola.h>
-// #include <MD_MAX72xx.h>
-// #include <SPI.h>
-
-// #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-// #define MAX_DEVICES   4
-// #define CS_PIN        10
-
-// MD_Parola display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
-
-// void renderText(const char* text) {
-//   display.displayClear();
-//   display.setTextAlignment(PA_CENTER);
-//   display.print(text);
-// }
-
-// void setup() {
-//   display.begin();
-//   display.setIntensity(1);
-//   display.displayClear();
-
-//   renderText("88888");
-// }
-
-// void loop() {
-//   // nothing yet
-// }
-
-
 // Including the required Arduino libraries
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
-// Uncomment according to your hardware type
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-//#define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
 
 // Defining size, and output pins
 #define MAX_DEVICES 4
