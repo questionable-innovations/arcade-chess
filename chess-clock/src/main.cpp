c412696b193f7e90c5a80c9736d67be3d25afed6
Arushi Gupta <arushigupta314159@gmail.com>
2026-07-16 20:57:24 +1200

    render text function for chess clock implemented

--- chess-clock/src/main.cpp
+++ chess-clock/src/main.cpp
@@ -12,22 +12,41 @@
 // Create a new instance of the MD_Parola class with hardware SPI connection
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
+void loop() {
+  if (myDisplay.displayAnimate()) {
+    myDisplay.displayReset();
+  }
+}
+
+void renderText(
+  const char* text,
+  textPosition_t align = PA_CENTER,     // left/center/right alignment
+  textEffect_t effect = PA_PRINT,       // PA_PRINT = static, PA_SCROLL_LEFT/RIGHT = scrolling
+  uint16_t speed = 100,                 // lower is faster
+  bool invert = false
+) {
+  myDisplay.setInvert(invert);
+  myDisplay.displayClear();
+
+  if (effect == PA_PRINT) {
+    myDisplay.setTextAlignment(align);
+    myDisplay.print(text);
+  } else {
+    myDisplay.displayScroll(text, align, effect, speed);
+  }
+}
+
+// To implement - timer, stopwatch, and text render functions
 
 void setup() {
   // Initialize the object
   myDisplay.begin();
 
   // Set the intensity (brightness) of the display (0-15)
-  myDisplay.setIntensity(0);
+  myDisplay.setIntensity(8);
 
   // Clear the display
   myDisplay.displayClear();
 
-  myDisplay.displayScroll("Hello", PA_CENTER, PA_SCROLL_LEFT, 100);
-}
-
-void loop() {
-  if (myDisplay.displayAnimate()) {
-    myDisplay.displayReset();
-  }
+  renderText("Welcome to Arcade Chess!", PA_CENTER, PA_SCROLL_LEFT);
 }
