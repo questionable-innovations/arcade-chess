2db95311f91bd2cf2378eaa14424ba378a99c288
Arushi Gupta <arushigupta314159@gmail.com>
2026-07-16 22:06:32 +1200

    Timer function implemented + render text clear bug fixed

--- chess-clock/src/main.cpp
+++ chess-clock/src/main.cpp
@@ -12,11 +12,16 @@
 // Create a new instance of the MD_Parola class with hardware SPI connection
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-void loop() {
-  if (myDisplay.displayAnimate()) {
-    myDisplay.displayReset();
-  }
+unsigned long timeLeft;
+unsigned long lastTick;
+unsigned long lastShownSec;
+
+void startTimer(unsigned long startMs) {
+  timeLeft = startMs;
+  lastTick = millis();
+  lastShownSec = ~0UL; // force first render
 }
+
 
 void renderText(
   const char* text,
@@ -26,17 +31,52 @@ void renderText(
   bool invert = false
 ) {
   myDisplay.setInvert(invert);
-  myDisplay.displayClear();
 
   if (effect == PA_PRINT) {
-    myDisplay.setTextAlignment(align);
-    myDisplay.print(text);
+    // pause = 0xFFFF so text doesn't immediately clear
+    myDisplay.displayText(text, align, speed, 0xFFFF, PA_PRINT, PA_NO_EFFECT);
   } else {
     myDisplay.displayScroll(text, align, effect, speed);
   }
 }
 
-// To implement - timer, stopwatch, and text render functions
+void pumpDisplay() {
+  if (myDisplay.displayAnimate()) {
+    myDisplay.displayReset();
+  }
+}
+
+void holdFor(unsigned long ms) {
+  unsigned long start = millis();
+  while (millis() - start < ms) {
+    pumpDisplay();
+  }
+}
+
+void updateTimer() {
+  unsigned long now = millis();
+  unsigned long elapsed = now - lastTick;
+  lastTick = now;
+
+  if (timeLeft > elapsed) {
+    timeLeft -= elapsed;
+  } else {
+    timeLeft = 0;
+  }
+
+  unsigned long sec = timeLeft / 1000;
+  if (sec == lastShownSec) return;
+  lastShownSec = sec;
+
+  char buf[6];  // MM:SS\0
+  snprintf(buf, sizeof(buf), "%02lu:%02lu", sec / 60, sec % 60);
+  renderText(buf, PA_CENTER, PA_PRINT);
+}
+
+void loop() {
+  updateTimer();
+  pumpDisplay();
+}
 
 void setup() {
   // Initialize the object
@@ -45,8 +85,21 @@ void setup() {
   // Set the intensity (brightness) of the display (0-15)
   myDisplay.setIntensity(8);
 
-  // Clear the display
-  myDisplay.displayClear();
+  renderText("Welcome to Arcade Chess!", PA_CENTER, PA_SCROLL_LEFT, 50);
+  holdFor(6000);
+  renderText("HELLO");
+  holdFor(2000);
 
-  renderText("Welcome to Arcade Chess!", PA_CENTER, PA_SCROLL_LEFT);
+  renderText("LEFT", PA_LEFT);
+  holdFor(2000);
+
+  renderText("RIGHT", PA_RIGHT);
+  holdFor(2000);
+
+  renderText("05:00", PA_CENTER, PA_PRINT, 100, true);
+  holdFor(2000);
+
+  renderText("SCROLL >>", PA_LEFT, PA_SCROLL_LEFT, 50);
+  holdFor(6000);
+  startTimer(5UL * 60 * 1000); // 5 min
 }
